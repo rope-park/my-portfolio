4902918066513f2359bf2cb9aa73caeb292504d0
rope-park <kelly6789@naver.com>
2025-03-30 21:43:41 +0900

    Feat: Filter, Sort 컴포넌트 UI 및 로직 개선 - 필터: Button 컴포넌트 사용으로 UX 개선, 필터 상태 useEffect 최적화 - 정렬: Sort 기준 선택 UI 게선 및 선택값 전달 로직 최적화 - 스타일: 배경색 및 폰트 색상 css 변수 기반으로 통일

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,40 +1,44 @@
-// sort.tsx - 블로그 정렬 컴포넌트.
+// Sort.tsx - 블로그 정렬 컴포넌트.
 
 "use client";
 import React, { useState, useEffect } from "react";
 
+// 정렬 기준
 interface SortOption {
-    value: string;
-    label: string;
+    value: string; // 정렬 기준
+    label: string;  // 정렬 기준 라벨
 }
 
+// Sort 컴포넌트의 props
 interface SortProps {
-    availableOptions: SortOption[];
-    defaultSort?: string;
-    onSortChange: (sort: string) => void;
+    availableOptions: SortOption[]; // 정렬 기준 목록
+    defaultSort?: string; // 기본 정렬 기준
+    onSortChange: (sort: string) => void; // 정렬 기준 변경 시 호출할 함수
 }
 
+// 정렬 기준을 선택할 수 있는 셀렉트 박스 렌더링.
 function Sort({ availableOptions, defaultSort="", onSortChange }: SortProps) {
-    const [selectedSort, setSelectedSort] = useState<string>(defaultSort);
+    const [selectedSort, setSelectedSort] = useState(defaultSort);
 
+    // 정렬 기준 변경 시 호출할 함수
     useEffect(() => {
         onSortChange(selectedSort);
-        }, [selectedSort, onSortChange]);
+    }, [selectedSort, onSortChange]);
         
     return (
-        <div className="flex items-center space-x-2">
-            <label htmlFor="sort" className="text-gray-700 dark:text-gray-300">
+        <div className="flex items-center space-x-2 text-sm">
+            <label htmlFor="sort" className="font-medium text-gray-700">
                 Sort by:
             </label>
             <select
                 id="sort"
                 value={selectedSort}
                 onChange={(e) => setSelectedSort(e.target.value)}
-                className="px-3 py-2 text-gray-900 bg-white border border-gray-300 rounded-md dark:border-gray-600 dark:bg-gray-700 dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                className="px-3 py-2 rounded-md border bg-[rgb(var(--color-background))] text-[rgb(var(--color-text))] ring-1 ring-gray-300 focus:outline-none focus:ring-2 focus:ring-[rgb(var(--color-primary))] transition-all"
             >
-                {availableOptions.map((availableOptions) => (
-                    <option key={availableOptions.value} value={availableOptions.value}>
-                        {availableOptions.label}
+                {availableOptions.map(({ value, label }) => (
+                    <option key={value} value={value}>
+                        {label}
                     </option>
                 ))}
             </select>
