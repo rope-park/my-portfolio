ab68a71e2a8a1d1ece6dc0198e0023c94104192e
rope-park <kelly6789@naver.com>
2025-04-01 13:49:55 +0900

    Feat: 다크 모드 지원을 위한 ThemeProvider 설정 추가 - next-themes의 ThemeProvider에 enableSystem 옵션 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,8 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system">
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {/* ThemeProvider: 다크 모드 및 라이트 모드 지원을 위한 테마 제공자 */}
     <div className="grid grid-rows-[auto, 1fr, auto] w-full min-w-0 min-h-screen overflow-auto">
       <Header />
       <main className="px-6 py-6 pt-[60px]">{children}</main>
