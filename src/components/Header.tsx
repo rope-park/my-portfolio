6a2d37b3e5cfbb80698c995504846617a9079581
rope-park <kelly6789@naver.com>
2025-04-01 21:45:46 +0900

    Feat: useScrollDirection을 이용한 스크롤 상태 관리 및 header 애니메이션 추가 - 커스터마이징된 훅을 이용하여 스크롤 방향 추적 - 위로 스크롤 할 때 framer-motion을 이용해 헤더 숨김

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,28 @@
 // About, Projects, Blog, Contact 네비게이션 제공 및 이동.
 
 "use client"; // Next.js 13 이상부터 지원하는 기능. CSR을 명시적으로 사용(기본값 SSR).
+
 import Logo from "./Logo";
 import Navigation from "./Navigation";
 import Toggle from "./Toggle";
+import { useScrollDirection } from "../hooks/useScrollDirection";
+import { motion } from "framer-motion";
 
 const Header = () => {
+
+    const scrollDirection = useScrollDirection(); // 스크롤 방향을 감지하는 커스텀 훅
+
     return (
-        <header className="fixed top-0 left-0 z-50 flex items-center justify-between w-full h-16 px-6 shadow-md backdrop-blur-3xl bg-[rgb(var(--color-background))]/80">
+        <motion.header
+            initial={false}
+            animate={{
+                y: scrollDirection === "down" ? -100 : 0,
+                transition: {
+                    duration: 0.3,
+                    ease: "easeInOut",
+                },
+            }}
+            className="fixed top-0 left-0 z-50 flex items-center justify-between w-full h-16 px-6 shadow-md backdrop-blur-3xl bg-[rgb(var(--color-background))]/80">
             {/* 로고 컴포넌트 */}
             <div className="flex items-center w-1/3">
                 <Logo />
@@ -26,9 +41,9 @@ const Header = () => {
                 <div className="md:hidden">
                     <Navigation mobileOnly />
                 </div>
-                
+
             </div>
-        </header >
+        </motion.header>
     );
 };
 
